d25cf52e1d4cf9933bed5ae32f82372c91c7bbb9
furkan-cloud <firstinfirstout2020@gmail.com>
2020-10-18 17:41:27 +0300

    button and input added

---
+++ src/Main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+import {main} from './styles';
+import TodoInput from './components/TodoInput';
+
+/**
+ * #303e45
+ * #a7b6bd
+ * #4a636e
+ * #ff9b31
+ */
+
+const Main = () => {
+  return (
+    <SafeAreaView style={main.container}>
+      <View style={main.container}>
+        <View style={main.banner}>
+          <Text style={main.todoText}>TODO</Text>
+          <Text style={main.todoCount}>10</Text>
+        </View>
+        <TodoInput
+        onTodoEnter={todoText => alert(todoText)} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Main;
---
+++ src/styles.js
@@ -0,0 +1,53 @@
+import {Dimensions, StyleSheet} from 'react-native';
+
+const color = "#4e342e";
+
+const main = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1565c0',
+  },
+  banner: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    alignItems: "center"
+  },
+  todoText: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color
+  },
+  todoCount: {
+    fontSize: 30,
+    color 
+  },
+});
+
+const todo_input =StyleSheet.create({
+    inputContainer: {
+        backgroundColor: "#bbdefb",
+        padding: 5,
+        margin:15,
+        borderRadius: 10,
+    },
+    container: {
+        backgroundColor: "#64b5f6",
+    },
+    buttonContainer: {
+        backgroundColor: "red",
+        padding: 10,
+        margin: 10,
+        width: Dimensions.get("window").width / 2,
+        alignSelf: "center",
+        borderRadius: 8,
+    },
+    buttonText: {
+        color: "white",
+        textAlign: "center",
+        fontWeight: "bold",
+    }
+});
+
+
+export {main, todo_input};
